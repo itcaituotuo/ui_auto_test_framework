7095742f00d8adbbf2cb4d5ff3cb6841ab0f1f3c
itcaituotuo <291151689@qq.com>
2022-08-07 00:12:41 +0800

    优化

--- pages/userinfo_page.py
+++ pages/userinfo_page.py
@@ -2,6 +2,8 @@
 # 作者：测试蔡坨坨(caituotuo.top)
 # 时间：2022/6/测试蔡坨坨2 测试蔡坨坨2:4测试蔡坨坨
 # 功能：个人信息页
+
+
 import time
 
 from selenium.webdriver.common.by import By
@@ -13,7 +15,7 @@ class UserInfoPage(BasePage):
     def __init__(self, driver):
         super(UserInfoPage, self).__init__(driver)
 
-    userinfo_icon_loc = (By.XPATH, '//div[3]/div[2]/div[测试蔡坨坨]/ul/li[2]/a')
+    userinfo_icon_loc = (By.LINK_TEXT, '用户资料')
     nic_loc = (By.ID, 'usernick')
     save_loc = (By.ID, 'saveProfile')
     tip_loc = (By.ID, 'errorTips')
@@ -23,7 +25,8 @@ class UserInfoPage(BasePage):
         修改昵称并保存
         :return:
         """
-        self._click_button(self.userinfo_icon_loc, "用户信息tab")
+        time.sleep(2)
+        self._click_button(self.userinfo_icon_loc, "用户资料tab")
         self._send_keys(self.nic_loc, nic, "昵称输入框")
         self._click_button(self.save_loc, "保存按钮")
 
